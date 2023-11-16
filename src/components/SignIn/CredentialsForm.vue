4e947330c04d99cd711e4136876865f6abf12a50
Joao Pedro Lima Teixeira <joaopedrolimateixeira@outlook.com>
2023-11-16 01:02:17 -0300

    Validations sign in

--- src/components/SignIn/CredentialsForm.vue
+++ src/components/SignIn/CredentialsForm.vue
@@ -3,21 +3,42 @@
     <div class="signin-content mb-15">
       <div class="text-h3 font-weight-light">Credenciais</div>
       <div class="text-h6 mb-4 font-weight-light">Digite sua senha</div>
-      <v-text-field v-model="signInEmailInput" class="custom-disabled-input" variant="outlined" placeholder="E-mail"
-        append-inner-icon="mdi-pencil" @click:append-inner="handleEditClick" readonly>
+      <v-text-field
+        v-model="signInEmailInput"
+        class="custom-disabled-input"
+        label="E-mail"
+        variant="outlined"
+        append-inner-icon="mdi-pencil"
+        @click:append-inner="handleEditClick"
+        readonly
+      >
       </v-text-field>
 
-      <v-text-field v-model="passwordInputValue" variant="outlined" placeholder="Senha"></v-text-field>
+      <v-text-field
+        v-model="passwordInputValue"
+        :append-inner-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
+        :type="showPassword ? 'text' : 'password'"
+        label="Senha"
+        variant="outlined"
+        @click:append-inner="showPassword = !showPassword"
+      ></v-text-field>
 
-      <v-btn class="text-subtitle-1 font-weight-regular mb-4" color="primary" height="45px" width="100%" variant="flat"
-        :ripple="false">
+      <v-btn
+        @click="handleLogInClick"
+        class="text-subtitle-1 font-weight-regular mb-4"
+        color="primary"
+        height="45px"
+        width="100%"
+        variant="flat"
+        :ripple="false"
+      >
         Entrar
       </v-btn>
 
-      <div class="">
-        Novo por aqui?
+      <div class="d-flex flex-column">
+        Esqueceu sua senha?
         <a class="text-decoration-none" href="" style="color: #0d47a1">
-          Criar uma nova conta
+          Clique aqui para recuperar sua senha!
         </a>
       </div>
     </div>
@@ -29,6 +50,7 @@ import { ref } from "vue";
 import { useSignInStore } from "@/store/store";
 import { storeToRefs } from "pinia";
 import { useRouter } from "vue-router";
+import { sha256 } from 'js-sha256';
 
 const router = useRouter();
 
@@ -36,12 +58,20 @@ const signInStore = useSignInStore();
 const { signInEmailInput } = storeToRefs(signInStore);
 
 const passwordInputValue = ref("");
+const showPassword = ref(false);
 
 const handleEditClick = () => {
   router.push({
-    name: "Identifier"
+    name: "Identifier",
   });
-}
+};
+
+const handleLogInClick = () => {
+  console.log(sha256(passwordInputValue.value))
+  /*  router.push({
+    name: "Identifier",
+  }); */
+};
 </script>
 
 <style lang="scss">
@@ -65,8 +95,8 @@ const handleEditClick = () => {
   }
 
   .v-field__outline {
-    --v-field-border-opacity: 1 !important;
     --v-field-border-opacity: 0.38 !important;
+    --v-field-border-width: 1px !important;
   }
 }
 </style>
--- src/components/SignIn/IdentifierForm.vue
+++ src/components/SignIn/IdentifierForm.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="signin-form-container">
+  <v-form ref="identifierForm" validate-on="layz" class="signin-form-container">
     <div class="signin-content mb-15">
       <div class="text-h3 font-weight-light">Bem-vindo</div>
-      <div class="text-h6 mb-4 font-weight-light">Digite seu e-mail</div>
+      <div class="text-h6 mb-7 font-weight-light">Digite seu e-mail</div>
       <v-text-field
+        class="my-3"
         v-model="signInEmailInput"
         variant="outlined"
-        placeholder="E-mail"
+        label="E-mail"
+        :rules="emailRules"
+        @keyup.enter="handleContinueClick"
+        @keydown.enter.prevent
       >
       </v-text-field>
 
@@ -22,18 +26,18 @@
         Continuar
       </v-btn>
 
-      <div class="">
-        Novo por aqui?
+      <div class="d-flex flex-column">
+        É novo por aqui?
         <a class="text-decoration-none" href="" style="color: #0d47a1">
-          Criar uma nova conta
+          Clique aqui para criar sua conta!
         </a>
       </div>
     </div>
-  </div>
+  </v-form>
 </template>
 
 <script setup>
-/* import { ref } from "vue"; */
+import { ref } from "vue";
 import { useSignInStore } from "@/store/store";
 import { storeToRefs } from "pinia";
 import { useRouter } from "vue-router";
@@ -43,10 +47,29 @@ const router = useRouter();
 const signInStore = useSignInStore();
 const { signInEmailInput } = storeToRefs(signInStore);
 
-const handleContinueClick = () => {
-  setTimeout(() => {
-    router.push({ name: "Credentials" });
-  }, 100);
+const identifierForm = ref();
+
+const emailRules = [
+  (value) => {
+    if (value) return true;
+
+    return "E-mail não pode ser vazio.";
+  },
+  (value) => {
+    if (/.+@.+\..+/.test(value)) return true;
+
+    return "E-mail must be valid.";
+  },
+];
+
+const handleContinueClick = async () => {
+  const { valid } = await identifierForm.value.validate();
+
+  if (valid) {
+    setTimeout(() => {
+      router.push({ name: "Credentials" });
+    }, 100);
+  } else alert("invalido");
 };
 </script>
 
