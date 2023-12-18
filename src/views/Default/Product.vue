3af5af4c9995599ee37ff274e7f600d86ea6e4d3
Joao Pedro Lima Teixeira <joaopedrolimateixeira@outlook.com>
2023-12-18 01:28:15 -0300

    Product styling

--- src/views/Default/Product.vue
+++ src/views/Default/Product.vue
@@ -1,48 +1,191 @@
 <template>
-  <div class="product-view container-limit">
-    <div class="d-flex justify-center h-100 w-100" style="padding-top: 36px">
+  <div class="product-view container-limit container-size-padding">
+    <div class="product-main">
       <div class="product-left">
-        <div class="product-imagens h-100 w-100">
-          <div class="product-main-img h-100 w-100" style="max-height: 500px">
+        <div class="product-images h-100 w-100">
+          <div class="product-images-grid w-100">
             <img
-              style="object-fit: contain;"
-              class="h-100 w-100"
-              src="//tatuapeconceito.cdn.plataformaneo.com.br/produtograde/20231211173403_3685996315_G.webp"
+              class="product-img"
+              src="https://cdn-images.farfetch-contents.com/22/17/13/25/22171325_51919233_1000.jpg"
+            />
+            <img
+              class="product-img"
+              src="https://cdn-images.farfetch-contents.com/22/17/13/25/22171325_51919239_1000.jpg"
+            />
+            <img
+              class="product-img"
+              src="https://cdn-images.farfetch-contents.com/22/17/13/25/22171325_51919239_1000.jpg"
+            />
+            <img
+              class="product-img"
+              src="https://cdn-images.farfetch-contents.com/22/17/13/25/22171325_51919164_1000.jpg"
             />
           </div>
+          <!--  <div class="product-preview-container d-flex">
+                <div class="product-preview-img selected-preview-img">
+              <img
+                src="//tatuapeconceito.cdn.plataformaneo.com.br/produtograde/20231211173403_3685996315_G.webp"
+              />
+            </div>
+            <div v-for="preview in previewImages" class="product-preview-img">
+              <img :src="preview" />
+            </div>
+          </div> -->
         </div>
       </div>
-
       <div class="product-right">
-        <div class="product-desc">
-          <div class="text-h4">Kit 6 Tech T-Shirt</div>
-          <div class="product-price mt-6">
-            <div class="d-flex flex-column">
-              <div>R$ 899</div>
-              <div>Preço normal</div>
+        <div class="product-header pt-10">
+          <div
+            class="product-title font-weight-bold"
+            style="
+              font-size: 26px;
+              line-height: 1.3;
+              text-transform: uppercase !important;
+            "
+          >
+            Camiseta Mangas Curtas Com Estampa Medusa
+          </div>
+          <div class="product-price mt-1">
+            <div class="d-flex align-center">
+              <div class="text-h6 mr-4 font-weight-regular">R$24.90</div>
+              <div
+                class="text-body-1 font-weight-light"
+                style="margin-top: 2px"
+              >
+                <s>R$32</s>
+              </div>
             </div>
-            <div>R$ 954</div>
           </div>
         </div>
-        <div class="product-filter">v</div>
+        <!--    <div class="product-filter d-flex align-center">v</div> -->
         <div class="product-checkout mt-4">
-          <div class="product-amount">
-            <div>Quantidade:</div>
-            <div class="d-flex">
-              <v-btn density="compact" icon="mdi-plus" @click="amount--" />
-              <div class="px-5">
+          <div class="product-amount d-flex flex-column">
+            <div class="text-body-2 font-weight-light mb-2">Quantidade</div>
+            <div
+              class="d-flex"
+              style="
+                border: 1px solid #111111;
+                border-radius: 3px;
+                height: 44px;
+                width: fit-content;
+              "
+            >
+              <v-btn
+                class="h-100 px-0"
+                elevation="0"
+                @click="amount--"
+                :ripple="false"
+                style="min-width: 45px"
+              >
+                <v-icon>mdi-minus</v-icon>
+              </v-btn>
+
+              <div
+                class="d-flex justify-center align-center h-100"
+                style="width: 50px"
+              >
                 <div>
                   {{ amount }}
                 </div>
               </div>
-              <v-btn density="compact" icon="mdi-plus" @click="amount++" />
+
+              <v-btn
+                class="h-100 px-0"
+                elevation="0"
+                @click="amount++"
+                width="45px"
+                :ripple="false"
+                style="min-width: 45px"
+              >
+                <v-icon>mdi-plus</v-icon>
+              </v-btn>
             </div>
           </div>
-          <div class="product-amount mt-5">
-            <v-btn>Adicionar ao carrinho</v-btn>
+
+          <div>
+            <v-btn
+              class="font-weight-regular button-color button-black mt-3"
+              height="45px"
+              width="100%"
+              variant="flat"
+              :ripple="false"
+            >
+              ADICIONAR AO CARRINHO
+            </v-btn>
+          </div>
+
+          <div class="mt-7" style="font-size: 14.6667px">
+            Verde, jacquard, fechamento frontal por botão e zíper, cordão de
+            ajuste no cós, comprimento cropped, modelagem cenoura e cinco
+            bolsos. Ao comprar este item unissex, considere que sua grade de
+            tamanhos é masculina.
           </div>
         </div>
       </div>
+    </div>
+    <div class="product-details mt-10">
+      <v-list class="w-100">
+        <v-list-group value="details">
+          <template v-slot:activator="{ props }">
+            <v-list-item
+              v-bind="props"
+              title="Detalhes do Produto"
+              :ripple="false"
+            ></v-list-item>
+          </template>
+
+          <v-list-item
+            title="Verde, jacquard, fechamento frontal por botão e zíper, cordão de ajuste
+            no cós, comprimento cropped, modelagem cenoura e cinco bolsos. Ao
+            comprar este item unissex, considere que sua grade de tamanhos é
+            masculina."
+          ></v-list-item>
+        </v-list-group>
+
+        <v-list-group value="measurements">
+          <template v-slot:activator="{ props }">
+            <v-list-item
+              v-bind="props"
+              title="Medidas"
+              :ripple="false"
+            ></v-list-item>
+          </template>
+
+          <v-list-item class="px-0">
+            <div
+              class="w-100 h-100 d-flex"
+              style="height: 578.983px !important"
+            >
+              <div>
+                Verde, jacquard, fechamento frontal por botão e zíper, cordão de
+                ajuste no cós, comprimento cropped, modelagem cenoura e cinco
+                bolsos. Ao comprar este item unissex, considere que sua grade de
+                tamanhos é masculina.
+              </div>
+
+              <div class="product-details-img">
+                <img
+                  class="h-100"
+                  style="object-fit: contain"
+                  src="https://cdn-images.farfetch-contents.com/22/17/13/25/22171325_51919164_1000.jpg"
+                />
+              </div>
+            </div>
+          </v-list-item>
+        </v-list-group>
+      </v-list>
+
+      <!--  <div>
+        Verde, jacquard, fechamento frontal por botão e zíper, cordão de ajuste
+        no cós, comprimento cropped, modelagem cenoura e cinco bolsos. Ao
+        comprar este item unissex, considere que sua grade de tamanhos é
+        masculina.
+      </div>
+      <div class="product-details-img">
+        <img
+          src="https://tatuapeconceito.cdn.plataformaneo.com.br/produtograde/multifotos/20231211173404_9218990782_G.webp"
+        />
+      </div> -->
     </div>
   </div>
 </template>
@@ -56,6 +199,10 @@ const route = useRoute();
 const productId = computed(() => route.params.produtoId);
 
 const amount = ref(0);
+const previewImages = ref([
+  "https://tatuapeconceito.cdn.plataformaneo.com.br/produtograde/multifotos/20231211173403_9648990352_G.webp",
+  "https://tatuapeconceito.cdn.plataformaneo.com.br/produtograde/multifotos/20231211173404_9218990782_G.webp",
+]);
 
 watch(amount, () => {
   if (amount.value < 0) {
@@ -74,21 +221,66 @@ onMounted(() => {});
 .product-view {
   height: 100%;
   width: 100%;
+  padding-top: 40px;
+  padding-bottom: 40px;
 
-  .product-left {
-    width: 60% !important;
-  }
-
-  .product-right {
-    width: 40% !important;
-  }
-
-  .product-left {
+  .product-main {
     display: flex;
     justify-content: center;
-  }
+    width: 100%;
 
-  .product-right {
+    .product-left {
+      width: 60% !important;
+      display: flex;
+      justify-content: center;
+
+      .product-images {
+        margin-right: 1.8rem;
+
+        .product-images-grid {
+          display: grid;
+          grid-template-columns: repeat(12, 1fr);
+          grid-gap: 14px;
+
+          .product-img {
+            height: auto;
+            max-width: 100%;
+            grid-column: span 6;
+          }
+        }
+
+        .product-preview-container {
+          height: 150px;
+          display: flex;
+          justify-content: center;
+
+          .product-preview-img {
+            height: 100%;
+            cursor: pointer;
+            opacity: 0.4;
+            margin: 0 10px;
+
+            img {
+              height: 100%;
+              object-fit: contain;
+            }
+
+            &.selected-preview-img {
+              opacity: 1 !important;
+              border: 1px solid #111111;
+            }
+          }
+        }
+      }
+    }
+
+    .product-right {
+      width: 40% !important;
+    }
+
+    @media (max-width: $tablet) {
+      flex-direction: column-reverse;
+    }
   }
 
   .product-price {
