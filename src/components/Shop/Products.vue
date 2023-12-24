eb6962d2c10a666fed033eefd1fb09f50c2df871
Joao Pedro Lima Teixeira <joaopedrolimateixeira@outlook.com>
2023-12-24 15:50:48 -0300

    boot

--- src/components/Shop/Products.vue
+++ src/components/Shop/Products.vue
@@ -107,32 +107,6 @@
           </div>
         </div>
       </v-card>
-
-      <v-card
-        v-for="i in i"
-        class="product-card d-flex flex-column"
-        elevation="0"
-      >
-        <div class="product-card-top">
-          <img
-            src="https://www.insiderstore.com.br/cdn/shop/files/faxinaplus444.jpg?v=1700881557&width=533"
-            alt=""
-          />
-        </div>
-        <div class="product-card-bottom">
-          <div class="font-weight-bold">Macbook Air Pro</div>
-          <p
-            class="text-subtitle-2 font-weight-light"
-            style="margin-bottom: 1px"
-          >
-            Some small description for the product
-          </p>
-          <div class="product-card-price d-flex">
-            <div class="text-subtitle-2 font-weight-regular">R$589.99</div>
-            <!--      <div class="text-caption price-cents">99</div> -->
-          </div>
-        </div>
-      </v-card>
     </div>
   </div>
 </template>
@@ -168,11 +142,10 @@ const search = () => {
 
   .product-card {
     cursor: pointer;
-    width: calc(25% - 20px);
+    width: calc(25% - 15px);
 
     .product-card-top {
-      width: 310px;
-      height: 310px;
+      width: 100%;
 
       img {
         height: 100%;
@@ -187,14 +160,16 @@ const search = () => {
     }
   }
 
-  /*  &.container-grid {
-    @include grid(330px);
-  } */
+  @media (max-width: $tablet) {
+    .product-card {
+      width: calc(50% - 10px);
+    }
+  }
 
-  /*  &.container-flex {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 1.5rem;
-  } */
+  @media (max-width: $phone) {
+    .product-card {
+      width: 100%;
+    }
+  }
 }
 </style>
